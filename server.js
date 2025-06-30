78ea2f450ca40cbd424bdb9a2cf4ff301e40f62d
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-30 14:21:08 +1000

    storing registered details

--- server.js
+++ server.js
@@ -9,13 +9,44 @@
 // console.log("this is the node start");
 
 import express from "express";
-
+import fs from "fs";
 const app = express();
+//middleware needed to display data captured
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Dented Code Academy");
+  res.sendFile("/Users/manish/Projects/node-start/pages/home.html");
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile("/Users/manish/Projects/node-start/pages/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile("/Users/manish/Projects/node-start/pages/register.html");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  let main = "Manish";
+  console.log(main);
+
+  // read file
+  let data = fs.readFileSync("./data/users.json");
+  console.log(JSON.parse(data));
+  let userList = JSON.parse(data);
+  userList.push(req.body);
+
+  //write to file
+  fs.writeFile("./data/users.json", JSON.stringify(userList), (err) => {
+    if (err) {
+      console.log("WRITE ERROR");
+    }
+  });
+
+  res.sendFile("/Users/manish/Projects/node-start/pages/register.html");
+});
 app.listen(3000, () => {
   console.log("The server is started at port:", 3000);
 });
