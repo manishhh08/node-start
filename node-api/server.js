a19ca7f389cf525b92544f4ca1d637479028ffb3
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-07-01 11:17:40 +1000

    api

--- node-api/server.js
+++ node-api/server.js
@@ -1,29 +1,33 @@
 import express from "express";
 const app = express();
-
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+let users = [
+  { id: 1, name: "ram" },
+  { id: 2, name: "Joe" },
+];
 app.get("/", (req, res) => {
   res.json({
     status: true,
-    message: "Hello Dented Code",
+    message: "HELLO WORLD",
   });
 });
-
+// GET all users
 app.get("/api/v1/users", (req, res) => {
+  //   return res.json({
+  //     status: false,
+  //     message: "Users not found",
+  //   });
   res.json({
     status: true,
-    message: "Hello Dented Code",
-    users: [
-      { id: 1, name: "manish" },
-      { id: 2, name: "lama" },
-    ],
+    message: "Users Found",
+    users: users,
   });
 });
-
+// Get specific user
 app.get("/api/v1/users/:userid", (req, res) => {
   let id = req.params.userid;
-
-  console.log("userid:", id);
-
+  console.log("Userid: ", id);
   let user = users.find((u) => u.id == id);
   if (user) {
     res.json({
@@ -31,15 +35,46 @@ app.get("/api/v1/users/:userid", (req, res) => {
       message: "User found",
       user: user,
     });
+  } else {
+    res.json({
+      status: false,
+      message: "User not found",
+    });
   }
-  //   res.json({
-  //     status: true,
-  //     message: "Hello Dented Code",
-  //     users: [
-  //       { id: 1, name: "manish" },
-  //       { id: 2, name: "lama" },
-  //     ],
-  //   });
 });
-
+// Create user
+app.post("/api/v1/users", (req, res) => {
+  // req.body;
+  let user = req.body;
+  users.push(user);
+  console.log(user);
+  res.json({
+    status: true,
+    message: "User Created",
+    users,
+  });
+});
+// update user
+app.patch("/api/v1/users/:userid", (req, res) => {
+  let id = req.params.userid;
+  let updatedData = req.body;
+  // { name: 'ghanshyam' }
+  let user = users.find((u) => u.id == id);
+  user.name = updatedData.name;
+  res.json({
+    status: true,
+    message: "User updated",
+    user,
+  });
+});
+// delete user
+app.delete("/api/v1/users/:userid", (req, res) => {
+  let id = req.params.userid;
+  //   filter user with id
+  users = users.filter((u) => u.id != id);
+  res.json({
+    status: true,
+    message: "User Deleted",
+  });
+});
 app.listen(3000);
