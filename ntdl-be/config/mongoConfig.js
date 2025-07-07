7e97be81aca2b231929c2c14d8b97422b821d8b1
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-07-07 20:54:33 +1000

    ntdl be create

---
+++ ntdl-be/config/mongoConfig.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongodbConnection = () => {
+  mongoose.connect(process.env.MONGO_URL);
+};
+
+export default mongodbConnection;
--- ntdl-be/server.js
+++ ntdl-be/server.js
@@ -1,34 +1,49 @@
 import express from "express";
+import mongoose from "mongoose";
+import mongodbConnection from "./config/mongoConfig.js";
 import cors from "cors";
+
+mongodbConnection();
 const app = express();
 //allows cors for cross domain use
 app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-let tasks = [];
+
+const taskSchema = new mongoose.Schema({
+  task: { type: String, required: true },
+  hour: { type: Number, required: true },
+  type: { type: String, required: true },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
 app.get("/", (req, res) => {
   res.json({
     status: true,
-    message: "HELLO WORLD",
+    message: "Task successfully added",
   });
 });
-// GET all users
-app.get("/api/v1/tasks", (req, res) => {
+// GET all tasks
+app.get("/api/v1/tasks", async (req, res) => {
   //   return res.json({
   //     status: false,
   //     message: "Users not found",
   //   });
+
+  let tasks = await Task.find();
   res.json({
     status: true,
     message: "Task Found",
     tasks: tasks,
   });
 });
-// Get specific user
-app.get("/api/v1/tasks/:taskid", (req, res) => {
+// Get specific task
+app.get("/api/v1/tasks/:taskid", async (req, res) => {
   let id = req.params.taskid;
-  console.log("taskid: ", id);
-  let task = tasks.find((u) => u.id == id);
+  //console.log("taskid: ", id);
+  let task = await Task.findById(id);
+  //let task = tasks.find((u) => u.id == id);
   if (task) {
     res.json({
       status: true,
@@ -43,25 +58,30 @@ app.get("/api/v1/tasks/:taskid", (req, res) => {
     });
   }
 });
-// Create user
-app.post("/api/v1/tasks", (req, res) => {
+// Create task
+app.post("/api/v1/tasks", async (req, res) => {
   // req.body;
-  let task = req.body;
-  tasks.push(task);
-  console.log(task);
+  // let task = req.body;
+  // tasks.push(task);
+  // console.log(task);
+
+  let task = new Task(req.body);
+  await task.save();
   res.json({
     status: true,
     message: "Task Created",
-    tasks,
+    task,
   });
 });
 // update user
-app.patch("/api/v1/tasks/:taskid", (req, res) => {
+app.patch("/api/v1/tasks/:taskid", async (req, res) => {
   let id = req.params.taskid;
   let updatedData = req.body;
   // { name: 'ghanshyam' }
-  let task = tasks.find((u) => u.id == id);
-  task.type = updatedData.type;
+  // let task = tasks.find((u) => u.id == id);
+  // task.type = updatedData.type;
+
+  let task = await Task.findByIdAndUpdate(id, updatedData, { new: true });
   res.json({
     status: true,
     message: "Task updated",
@@ -69,13 +89,15 @@ app.patch("/api/v1/tasks/:taskid", (req, res) => {
   });
 });
 // delete user
-app.delete("/api/v1/tasks/:taskid", (req, res) => {
+app.delete("/api/v1/tasks/:taskid", async (req, res) => {
   let id = req.params.taskid;
   //   filter user with id
-  tasks = tasks.filter((u) => u.id != id);
+  //tasks = tasks.filter((u) => u.id != id);
+  let deletedTask = await Task.findByIdAndDelete(id);
   res.json({
     status: true,
     message: "Task Deleted",
+    deletedTask,
   });
 });
 app.listen(3000);
